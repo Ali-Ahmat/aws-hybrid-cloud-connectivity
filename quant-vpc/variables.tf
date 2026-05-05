36f4600be898c3f796a9f53bbb80a5ee42e2e5a8
Ali-Ahmat <alikahmat14@gmail.com>
2026-05-05 18:56:46 -0400

    modifying root module  variables.tf

--- quant-vpc/variables.tf
+++ quant-vpc/variables.tf
@@ -1,9 +1,44 @@
-variable "region" {}
-variable "project_name" {}
-variable "vpc_cidr" {}
-variable "public_subnet_az1_cidr" {}
-variable "public_subnet_az2_cidr" {}
-variable "private_app_subnet_az1_cidr" {}
-variable "private_app_subnet_az2_cidr" {}
-variable "private_data_subnet_az1_cidr" {}
-variable "private_data_subnet_az2_cidr" {}
+variable "region" {
+  description = "AWS region where resources will be deployed"
+  type        = string
+}
+
+variable "project_name" {
+  description = "Project name used for resource naming and tagging"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "public_subnet_az1_cidr" {
+  description = "CIDR block for the public subnet in Availability Zone 1"
+  type        = string
+}
+
+variable "public_subnet_az2_cidr" {
+  description = "CIDR block for the public subnet in Availability Zone 2"
+  type        = string
+}
+
+variable "private_app_subnet_az1_cidr" {
+  description = "CIDR block for the private application subnet in Availability Zone 1"
+  type        = string
+}
+
+variable "private_app_subnet_az2_cidr" {
+  description = "CIDR block for the private application subnet in Availability Zone 2"
+  type        = string
+}
+
+variable "private_data_subnet_az1_cidr" {
+  description = "CIDR block for the private data subnet in Availability Zone 1"
+  type        = string
+}
+
+variable "private_data_subnet_az2_cidr" {
+  description = "CIDR block for the private data subnet in Availability Zone 2"
+  type        = string
+}
