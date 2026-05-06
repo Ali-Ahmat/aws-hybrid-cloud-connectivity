b82b141b85fc236d4d9a20624bc164f695bc4a9f
Ali-Ahmat <alikahmat14@gmail.com>
2026-05-06 11:33:29 -0400

    add backends for diff env

--- github-oidc/backend.tf
+++ github-oidc/backend.tf
@@ -1,7 +1,26 @@
+# environments/prod/backend.tf
 terraform {
   backend "s3" {
     bucket = "quant-terraform-remote-state"
     key    = "github-oidc.tfstate"
+    region = "us-east-1"
+  }
+}
+
+# environments/dev/backend.tf
+terraform {
+  backend "s3" {
+    bucket = "quant-terraform-remote-states"
+    key    = "dev/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+# environments/test/backend.tf
+terraform {
+  backend "s3" {
+    bucket = "quant-terraform-remote-states"
+    key    = "test/terraform.tfstate"
     region = "us-east-1"
   }
 }
