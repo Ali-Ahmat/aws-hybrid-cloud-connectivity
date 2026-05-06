31d9e7663f1beff61f20ae153c8b380c1efb5aff
Ali-Ahmat <alikahmat14@gmail.com>
2026-05-06 11:03:09 -0400

    refactor: replace empty environments/prod with existing prod root module

--- prod/backend.tf
+++
@@ -1,8 +0,0 @@
-# store the terraform state file in s3
-terraform {
-  backend "s3" {
-    bucket = "quant-terraform-remote-states"
-    key    = "quant-vpc.tfstate"
-    region = "us-east-1"
-  }
-}
